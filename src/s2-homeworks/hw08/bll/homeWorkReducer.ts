38ef972e31c84a751fa672effe963a41c2b21c07
petr1k27 <petr.1k2794@gmail.com>
2024-11-20 21:06:42 +0100

    sprint 3. Fourth week(HW6)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            const prevState = [...state];
+            if(action.payload === 'up') {
+                return prevState.sort((f,s) => f.name.localeCompare(s.name));
+            }
+            if(action.payload === 'down') {
+                return prevState.sort((f,s) => s.name.localeCompare(f.name));
+            }
 
             return state // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= action.payload)  // need to fix
         }
         default:
             return state
